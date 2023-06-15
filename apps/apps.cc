1c0fd2a9a5e7980e5c711975eb2bf1eafded990e
Iman Datta <idatta@uw.edu>
2023-06-15 16:14:49 -0700

    Move apps and writeoutdiagnostic to own file

---
+++ apps/apps.cc
@@ -0,0 +1,54 @@
+#include "apps.h"
+
+// app0
+const std::shared_ptr<WriteoutDiagnostic> App0::call(const std::array<real, 3>& x) const
+{
+    // Do the work of calculating physics
+
+    // Calculate a timestep
+    // For this test, generate a random number between 0 and 1
+    // https://en.cppreference.com/w/cpp/numeric/random/rand
+    // https://www.digitalocean.com/community/tutorials/random-number-generator-c-plus-plus
+    // https://stackoverflow.com/questions/686353/random-float-number-generation
+    // int rank;
+    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // const real lo = 0;
+    // const real hi = 1.0;
+    // real dt = lo + static_cast <real> (rand()) /( static_cast <real>
+    // (RAND_MAX/(hi-lo))); std::cout << ")rank = " << rank <<  " dt = " << dt
+    // 		  <<  " x = (" << x[0] << ", " << x[1] << ", " << x[2]
+    // 		  << ") physics = " << physics << std::endl;
+
+    const real dt = 1.0;
+    // return a timestep
+    _wd->setValues(dt, x, _physics);
+
+    return _wd;
+}
+
+// app1
+const std::shared_ptr<WriteoutDiagnostic> App1::call(const std::array<real, 3>& x) const
+{
+    // Do the work of calculating physics
+
+    // const real dt = x[0] * x[0] - 6;
+    const real dt = std::sin(2 * 3.141592654 * x[0] / 1);
+    // return a timestep
+    _wd->setValues(dt, x, _physics);
+
+    return _wd;
+}
+
+// app2
+const std::shared_ptr<WriteoutDiagnostic> App2::call(const std::array<real, 3>& x) const
+{
+    {
+        // Do the work of calculating physics
+
+        const real dt = 0.5 * std::sqrt(x[0]);
+        // return a timestep
+        _wd->setValues(dt, x, _physics);
+
+        return _wd;
+    }
+}
---
+++ apps/apps.h
@@ -0,0 +1,162 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <array>
+#include <string>
+#include <vector>
+#include <limits>
+#include <ctime>
+#include <cmath>
+
+
+
+typedef double real;
+
+class WriteoutDiagnostic
+{
+private:
+    real _dt;
+    std::array<real, 3> _x;
+    std::string _physics;
+
+public:
+    // Constructor
+    WriteoutDiagnostic()
+    {
+    }
+    // Constructor
+    WriteoutDiagnostic(real dt, const std::array<real, 3>& x, const std::string& physics)
+        : _dt(dt), _x(x), _physics(physics)
+    {
+    }
+
+    // Minimum dt
+    static const WriteoutDiagnostic& minDt(const WriteoutDiagnostic& a,
+                                           const WriteoutDiagnostic& b)
+    {
+        return a._dt < b._dt ? a : b;
+    }
+
+    // Setters
+    void setDt(real dt)
+    {
+        _dt = dt;
+    }
+    void setX(const std::array<real, 3>& x)
+    {
+        _x = x;
+    }
+    void setPhysics(const std::string& physics)
+    {
+        _physics = physics;
+    }
+
+    void setValues(real dt, const std::array<real, 3>& x, const std::string& physics)
+    {
+        setDt(dt);
+        setX(x);
+        setPhysics(physics);
+    }
+
+    // Getters
+    real getDt() const
+    {
+        return _dt;
+    }
+
+    const std::array<real, 3>& getX() const
+    {
+        return _x;
+    }
+
+    const std::string& getPhysics() const
+    {
+        return _physics;
+    }
+};
+
+class AppBase
+{
+protected:
+    std::shared_ptr<WriteoutDiagnostic> _wd;
+    std::string _physics;
+
+public:
+    // Constructor
+    AppBase()
+    {
+        _wd = std::make_shared<WriteoutDiagnostic>();
+    }
+
+    virtual void setup()
+    {
+    }
+
+    virtual const std::shared_ptr<WriteoutDiagnostic>
+    call(const std::array<real, 3>& x) const
+    {
+        std::cerr << "Unimplemented call()\n";
+        exit(-1);
+    }
+};
+
+class App0 : public AppBase
+{
+private:
+public:
+    // Constructor
+    App0()
+    {
+        _physics = "app0";
+    }
+
+    // Setup
+    void setup() override
+    {
+        AppBase::setup();
+    }
+
+    const std::shared_ptr<WriteoutDiagnostic>
+    call(const std::array<real, 3>& x) const override;
+};
+
+class App1 : public AppBase
+{
+private:
+public:
+    // Constructor
+    App1()
+    {
+        _physics = "app1";
+    }
+
+    // Setup
+    void setup() override
+    {
+        AppBase::setup();
+    }
+
+    const std::shared_ptr<WriteoutDiagnostic>
+    call(const std::array<real, 3>& x) const override;
+};
+
+class App2 : public AppBase
+{
+private:
+public:
+    // Constructor
+    App2()
+    {
+        _physics = "app2";
+    }
+
+    // Setup
+    void setup() override
+    {
+        AppBase::setup();
+    }
+
+    const std::shared_ptr<WriteoutDiagnostic>
+    call(const std::array<real, 3>& x) const override;
+};
